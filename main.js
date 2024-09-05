8bbe221ff25373832ca3bf3e76c7b23e261a32e2
diessicamattos <164754833+diessicamattos@users.noreply.github.com>
2024-09-05 09:28:11 -0300

    atualizado

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let trilho = document.getElementById('trilho')
+let main = document.querySelector('main')
+let header = document.querySelector('header')
+
+trilho.addEventListener('click', ()=>{
+    trilho.classList.toggle('dark')
+    main.classList.toggle('dark')
+    header.classList.toggle('dark')
+})
+
